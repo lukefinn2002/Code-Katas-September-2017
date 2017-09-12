61bde98d50fb066e8c31859b0bd06f0f11783741
lukefinn2002 <31880834+lukefinn2002@users.noreply.github.com>
2017-09-12 08:58:04 +0100

    Add files via upload

---
+++ ALeapYearKataShould.cs
@@ -0,0 +1,46 @@
+﻿
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace TotallyAwesomeLeapYearKata
+{
+    [TestFixture]
+    public class ALeapYearKataShould
+    {
+        [TestCase(1996, "true")]
+        [TestCase(2001, "false")]
+        [TestCase(1900, "false")]
+        [TestCase(2000, "true")]
+        [TestCase(1600, "true")]
+        [TestCase(1700, "false")]
+        [TestCase(2400, "true")]
+
+        public void ReturnTrueForLeapYears(int year, string expected)
+        {
+            var leapYear = new LeapYear();
+            var result = leapYear.ReturnTypeOfYear(year);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+
+    public class LeapYear
+    {
+        public string ReturnTypeOfYear(int year)
+        {
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                return "true";
+            }
+            if(year % 4 == 0 && year % 400 == 0)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+            
+        }
+    }
+}
